d941d151f8e35ac17485604d3f1030ac493de8eb
Adrian <adrianp1447@gmail.com>
2022-06-17 18:06:51 -0400

    through 7

--- indexPracticeFile.js
+++ indexPracticeFile.js
@@ -61,16 +61,55 @@
 // const output = capitalizeLetters('i love javascript');
 // console.log(output);
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
 
-function maxCharacter(str) {
+// function maxCharacter(str) {
+//     const charMap = {};
+//     let maxNum = 0;
+//     let maxChar = '';
 
+//     // turn into array for each letter put into map
+//     str.split('').forEach(char => charMap[char] ? charMap[char]++ : charMap[char] = 1);
 
-}
+//     //after map done this loops to find max num
+//     for (const char in charMap) {
+//         if (charMap[char] > maxNum) {
+//             maxNum = charMap[char]
+//             maxChar = char;
+//         }
+//     }
+//     return maxChar;
 
-const output = maxCharacter('javascript');
-console.log(output);
+// }
+
+// const output = maxCharacter('javaaaaassccript');
+// console.log(output);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3, 
+// instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are 
+// multiples of both 3 and 5, print "FizzBuzz".
+
+// function fizzBuzz() {
+//     for (let i = 1; i <= 100; i++) {
+//         //if remainder is 0
+//         if (i % 3 == 0) {
+//             console.log('Fizz');
+//         } else if (i % 5 == 0) {
+//             console.log('Buzz');
+//         } else if (i % 3 == 0 && i % 5 == 0) {
+//             console.log('FizzBuzz');
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// const output = fizzBuzz();
+// console.log(output);
